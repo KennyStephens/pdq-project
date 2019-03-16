efa774bbdee4f19bf26e848ef032ee0b562eb92b
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-15 21:34:16 -0600

    Init Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://kenkneesteefens:Nodecourse@cluster0-drydi.mongodb.net/node-passport'
+};
---
+++ routes/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+const fetch = require('node-fetch');
+var HTMLParser = require('node-html-parser');
+
+// Home Page
+router.get("/", (req, res) => {
+  fetch('https://pdqemployees.azurewebsites.net/api/pdqemployees')
+    .then(res => res.json())
+    .then(response => {
+      // Getting image src
+      fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
+        return imgResponse.text();
+      }).then((string) => {
+
+        const imageURL = HTMLParser.parse(string);
+        const imageArray = Array.from(imageURL.querySelectorAll('img'));
+        imageArray.forEach(src => {
+          if (src.rawAttrs.toString().includes(response.name) && src.rawAttrs.toString().includes('company')) {
+            // console.log(src.rawAttrs.split('/n')[0]);
+            const imageSrcParsed = src.rawAttrs.split('/n')[0];
+          }
+        });
+      }) // End of image src
+      res.render("home", {
+        employee: response
+      })
+    })
+    .catch(err => console.log(err))
+});
+
+
+module.exports = router;
