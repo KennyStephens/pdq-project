fd45870df335cf6a5ffd1076264b1fcf4afbf556
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-15 22:08:34 -0600

    messing wiht ideas

--- routes/index.js
+++ routes/index.js
@@ -15,17 +15,23 @@ router.get("/", (req, res) => {
 
         const imageURL = HTMLParser.parse(string);
         const imageArray = Array.from(imageURL.querySelectorAll('img'));
+        let test;
         imageArray.forEach(src => {
           if (src.rawAttrs.toString().includes(response.name) && src.rawAttrs.toString().includes('company')) {
             // console.log(src.rawAttrs.split('/n')[0]);
             const imageSrcParsed = src.rawAttrs.split('/n')[0];
+            test = imageSrcParsed;
           }
         });
       }) // End of image src
+      console.log(imageSrcParsed);
       res.render("home", {
         employee: response
       })
     })
+
+
+
     .catch(err => console.log(err))
 });
 
