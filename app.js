7ab048f2f5edc0668fd95dab1710ae5e3c3319fc
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-16 20:48:43 -0600

    FIXING IMAGE ISSUE!!!!!!

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const expressLayouts = require("express-ejs-layouts");
 
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 // EJS
 app.use(expressLayouts);
@@ -13,6 +15,12 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/", require("./routes/index"));
 
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+http.listen(PORT, function(){
+  console.log(`Listening on port ${PORT}`);
+});
--- routes/index.js
+++ routes/index.js
@@ -8,26 +8,36 @@ router.get("/", (req, res) => {
   fetch('https://pdqemployees.azurewebsites.net/api/pdqemployees')
     .then(res => res.json())
     .then(response => {
+      // console.log(response)
       // Getting image src
-      fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
+      const thing = fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
         return imgResponse.text();
       }).then((string) => {
-
         const imageURL = HTMLParser.parse(string);
         const imageArray = Array.from(imageURL.querySelectorAll('img'));
         imageArray.forEach(src => {
           if (src.rawAttrs.toString().includes(response.name) && src.rawAttrs.toString().includes('company')) {
             // console.log(src.rawAttrs.split('/n')[0]);
-            const imageSrcParsed = src.rawAttrs.split('/n')[0];
+            const imageSrcParsed = src.rawAttrs.split('/n')[0];   
+            // console.log(imageSrcParsed.split('"')[1]);       
+            response.image = imageSrcParsed.split('"')[1];
+            
+            // console.log(response);
+            res.render("home", {
+              employee: response
+            })
           }
         });
-      }) // End of image src
-      res.render("home", {
-        employee: response
+
       })
+      .catch(err => console.log(err)); // End of image src
     })
+
+
+
     .catch(err => console.log(err))
 });
 
 
 module.exports = router;
+
