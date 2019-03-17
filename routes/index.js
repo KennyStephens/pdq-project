ae83b98d0641caf2b69645d2f5713dad8ffd1cc0
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-16 21:05:55 -0600

    Cleaning up

--- routes/index.js
+++ routes/index.js
@@ -8,21 +8,18 @@ router.get("/", (req, res) => {
   fetch('https://pdqemployees.azurewebsites.net/api/pdqemployees')
     .then(res => res.json())
     .then(response => {
-      // console.log(response)
       // Getting image src
       const thing = fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
         return imgResponse.text();
       }).then((string) => {
         const imageURL = HTMLParser.parse(string);
         const imageArray = Array.from(imageURL.querySelectorAll('img'));
+        
         imageArray.forEach(src => {
           if (src.rawAttrs.toString().includes(response.name) && src.rawAttrs.toString().includes('company')) {
-            // console.log(src.rawAttrs.split('/n')[0]);
-            const imageSrcParsed = src.rawAttrs.split('/n')[0];   
-            // console.log(imageSrcParsed.split('"')[1]);       
+            const imageSrcParsed = src.rawAttrs.split('/n')[0];        
             response.image = imageSrcParsed.split('"')[1];
             
-            // console.log(response);
             res.render("home", {
               employee: response
             })
