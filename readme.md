9efd821eaeacb5868035a4c3846c85a2a97dde33
Kenny Stephens <kennethcstephens@gmail.com>
2019-03-17 10:03:37 -0600

    Added Readme

---
+++ readme.md
@@ -0,0 +1,12 @@
+# PDQ Employee API Technical Exercise
+***
+[Find live version of exercise here](https://pdq-assignment.herokuapp.com)
+
+This exercise was done using Nodejs, Express, EJS, Node Fetch, and Node HTML Parser.
+
+### Tasks 
+- [x] Fetch Employee Name and Favorite Beer
+- [x] Reach out to 'About Us' page, parse HTML, find image sources
+- [x] Render name, favorite beer, and image to front end (Used server side rendered pages with EJS)
+- [x] Update all front end clients when API call button is clicked (Used SocketIO to listen on updated and update all client side pages open)
+- [x] Error handling
--- routes/index.js
+++ routes/index.js
@@ -5,21 +5,23 @@ var HTMLParser = require('node-html-parser');
 
 // Home Page
 router.get("/", (req, res) => {
+  // Fetch employee and fav drink
   fetch('https://pdqemployees.azurewebsites.net/api/pdqemployees')
     .then(res => res.json())
     .then(response => {
-      // Getting image src
-      const thing = fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
+      // Grabbing HTML from about us page and parsing the data to get image source
+      fetch('https://www.pdq.com/about-us/').then((imgResponse) => {
         return imgResponse.text();
       }).then((string) => {
         const imageURL = HTMLParser.parse(string);
         const imageArray = Array.from(imageURL.querySelectorAll('img'));
 
+        // Comparing employee name with image src
         imageArray.forEach(src => {
           if (src.rawAttrs.toString().includes(response.name) && src.rawAttrs.toString().includes('company')) {
             const imageSrcParsed = src.rawAttrs.split('/n')[0];
             response.image = imageSrcParsed.split('"')[1];
-
+            // Rendering page with name, beer, and correct image src
             res.render("home", {
               employee: response
             })
@@ -28,6 +30,7 @@ router.get("/", (req, res) => {
       })
     })
     .catch(err => {
+      // Handling seldom HTML parsing error
       if(err.type === 'invalid-json') {
         res.redirect('/');
       }
